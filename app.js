e7edfa89139c858b3dad9feb8bdd40b86a3b4d6f
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2022-05-20 12:00:46 +0200

    carousel
    carousel fini

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
-const url = "https://gateway.marvel.com:443/v1/public/comics/91620?ts=1&apikey=c14df808edaae165c487253759fe027d&hash=b772d3866604177691f5c2b041434d8c";
-const img = document.getElementById('image');
-const body = document.querySelector('body');
-const script = document.querySelector('script')
-fetch(url)
-.then((resp) => resp.json())
-.then((data) => {
-    const image = document.querySelector('img');
-    image.src = data.data.results[0].thumbnail.path+".jpg";
-    console.log(data.data.results[0].thumbnail.path+"."+data.data.results[0].thumbnail.extension);
+let t = 0;
+let p = 0;
+const carousel = () => {
+  const items = document.querySelector(".carousel-item");
+  items.classList.add("active");
+  let total = document.querySelectorAll(".carousel-item");
 
-})
+  //   for (let index = 0; index < total.length; index++) {
+  //     let el = total[index];
+  //   }
+
+  let totalCa = total.length;
+
+  let current = 0;
+  const moveRight = document.querySelector("#moveRight");
+  const moveLeft = document.querySelector("#moveLeft");
+  moveRight.addEventListener("click", () => {
+    t++;
+    let next = current;
+    current = current + 1;
+    setSlide(next, current);
+  });
+
+  moveLeft.addEventListener("click", () => {
+    let prev = current;
+    current = current - 1;
+    setSlide(prev, current);
+  });
+
+  function setSlide(prev, next) {
+    let slide = current;
+    if (next > totalCa - 1) {
+      slide = 0;
+      current = 0;
+    }
+    if (next < 0) {
+      slide = totalCa - 1;
+      current = totalCa - 1;
+    }
+
+    const carousel = document.querySelectorAll(".carousel-item");
+    p = t + 1;
+    const currentCarousel = document.querySelector(".carousel-item--" + t);
+    if (t === 4) {
+      t = 1;
+      p = 1;
+    }
+
+    const nextCarousel = document.querySelector(".carousel-item--" + p);
+    console.log(currentCarousel);
+    currentCarousel.classList.remove("active");
+    nextCarousel.classList.add("active");
+
+    setTimeout(function () {}, 800);
+  }
+};
+carousel();
