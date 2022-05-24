0f65b8887660ea16d7339935d8f5e96a051834d4
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-05-24 16:51:50 +0200

    Modification du footer

--- script.js
+++ script.js
@@ -4,9 +4,13 @@
 const url =
   "https://gateway.marvel.com:443/v1/public/comics/91620?ts=1&apikey=bdfb2d1e4535176c82d3c714057f22f6&hash=427e8cb56a0d0612f1bcb9a42246507d";
 const lifeStory6 = document.querySelector(".lifeStory6");
+const avatar = document.querySelector('.round');
+
 fetch(url)
   .then((resp) => resp.json())
   .then((data) => {
+    console.log(data.data.results);
+    
     /* lifeStory6.src = data.data.results[0].images[0].path + ".jpg"; */
   });
 
